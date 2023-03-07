83a21ea3ba66b7f7d81c98de97289e29bf5614c1
NaufalAryaRamadhan <100133797+Naufalara@users.noreply.github.com>
2023-03-07 21:54:52 +0700

    Update OperasiTitik.java

--- Pertemuan 2/OperasiTitik.java
+++ Pertemuan 2/OperasiTitik.java
@@ -2,16 +2,25 @@
 //Pembuat: Naufal Arya Ramadhan
 //Deskripsi: kelas yang berisi method untuk merefleksikan sumbu x dan y
 public class OperasiTitik {
-    public void refleksiSumbuX(titik titik){
+    private void refleksiSumbuX(titik titik){
         double y;
         y = titik.getOrdinat();
         y = y * -1;
         titik.setOrdinat(y);
     }
-    public void refleksiSumbuY(titik titik){
+    private void refleksiSumbuY(titik titik){
         double x;
         x = titik.getAbsis();
         x = x * -1;
         titik.setAbsis(x);
+    }
+    public Titik refleksiX(Titik titik){
+        refleksiSumbuX(titik);
+        return titik;
+    }
+
+    public Titik refleksiY(Titik titik){
+        refleksiSumbuY(titik);
+        return titik;
     }
 }
